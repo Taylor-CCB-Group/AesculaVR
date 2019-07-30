fa77920b930f51a2d0d3c06c76a3af449f9ae1a8
Michael <MichaelOliverBabelVR@gmail.com>
2019-07-30 11:30:05 +0100

    Adding IFile and File, So we can represent files.

---
+++ Assets/AesculaVR/Scripts/File/File.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class File : IFile
+{
+
+    private DateTime accessed;
+    private DateTime created;
+    private DateTime modified;
+    
+    private string path;
+
+    public File(string path)
+    {
+        if (!System.IO.File.Exists(path))
+            throw new System.IO.FileNotFoundException("Unable to create file object for a file that does not exist", path);
+
+        this.path = path;
+
+
+        this.accessed = System.IO.File.GetLastAccessTime(path);
+        this.created  = System.IO.File.GetCreationTime  (path);
+        this.modified = System.IO.File.GetLastWriteTime (path);
+    }
+
+    public DateTime Accessed() => accessed;
+
+    public DateTime Created() => created;
+
+    public DateTime Modified() => modified;
+
+
+    public string Name(bool withExtension = true)
+    {
+        if (withExtension)
+            return System.IO.Path.GetFileName(path);
+        else
+            return System.IO.Path.GetFileNameWithoutExtension(path);
+    }
+
+    public string Path(bool withName = true)
+    {
+        if (withName)
+            return System.IO.Path.GetFullPath(path);
+        else
+            return System.IO.Path.GetDirectoryName(this.Path(true));
+    }
+}
---
+++ Assets/AesculaVR/Scripts/File/IFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  Then interface for every file
+/// </summary>
+public interface IFile
+{
+  
+    /// <summary>
+    /// The name of the file.
+    /// </summary>
+    /// <param name="withExtension">does the name include the file extension. e.g. ".txt"</param>
+    /// <returns>The files name.</returns>
+    string Name(bool withExtension = true);    
+
+    /// <summary>
+    /// The absolute path of the file.
+    /// </summary>
+    /// <param name="withName">Do we want the filename (with extension) included?</param>
+    /// <returns>the absolute path of the file.</returns>
+    string Path(bool withName = true);
+    
+
+    /// <summary>
+    /// When was the file created?
+    /// </summary>
+    /// <returns>The DateTime for when the file was created </returns>
+    DateTime Created ();
+    /// <summary>
+    ///  When was the file last modified?
+    /// </summary>
+    /// <returns>The DateTime for when the file was last modiefied</returns>
+    DateTime Modified();
+    /// <summary>
+    /// When was the file last accessed?
+    /// </summary>
+    /// <returns>The DateTime for when the file was last accessed</returns>
+    DateTime Accessed();
+}
