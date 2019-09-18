a625d23440e7f549b33dc3516345a1c438c77432
Michael <MichaelOliverBabelVR@gmail.com>
2019-09-18 14:20:35 +0100

    Added a way to change the scene.

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public enum SceneType { Editor, Recorder }
+
+    [SerializeField] private Dialog diaglog;
+    [SerializeField] private SceneType changeTo;
+
+    [SerializeField] private string editor, recorder;
+
+    public void Awake()
+    {
+        this.GetComponent<Button>().onClick.AddListener(OnPress);
+    }
+
+    public void OnPress()
+    {
+        diaglog.Show(ChangeScene(), "Change scene?", DialogMessage());
+    }
+
+    public UnityAction ChangeScene()
+    {
+        return () => { SceneManager.LoadScene(changeTo == SceneType.Editor ? editor : recorder); };  
+    }
+
+    public string DialogMessage()
+    {
+        switch (changeTo)
+        {
+            case SceneType.Editor:
+                return "Do you want to change to the editor scene.";
+            case SceneType.Recorder:
+                return "Do you want to change to the recording scene.";
+            default:
+                throw new System.NotImplementedException();
+        }
+    }
+}
